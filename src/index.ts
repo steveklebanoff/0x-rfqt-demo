95afc68a62075224ef513c1535bee32fbe5495c7
Steve Klebanoff <steve@0xproject.com>
2020-04-27 16:34:23 -0700

    start

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,47 @@
-const appEl = document.getElementById("app");
-if (appEl) {
-  appEl.innerHTML = "0x API demo";
-}
+const SELL_TOKEN = "WETH";
+const SELL_TOKEN_DECIMALS = 18;
+const SELL_UNIT_AMOUNT = 0.25;
+const BUY_TOKEN = "USDC";
+const SLIPPAGE_PERCENT = 0.03;
+const API_URL = "https://api.0x.org";
+
+(window as any).submitApiRequest = async () => {
+  const submitEl = document.getElementById("submit-request") as HTMLInputElement;
+  const apiKeyEl = document.getElementById('api-key') as HTMLInputElement;
+  const takerAddressEl = document.getElementById('taker-address') as HTMLInputElement;
+  const quoteTypeEl = document.getElementById('quote-type') as HTMLInputElement;
+  const resultsEl = document.getElementById("results")!;
+
+  const apiKey = apiKeyEl.value;
+  const takerAddress = takerAddressEl.value;
+  if (!apiKey || !takerAddress) {
+    alert('Please provide an API key and a taker address');
+    return;
+  }
+
+  submitEl.disabled = true;
+  resultsEl.innerText = "Submitting...";
+
+  const sellAmount = SELL_UNIT_AMOUNT * (10 ** SELL_TOKEN_DECIMALS);
+  let apiRequestUrl: string;
+  if (quoteTypeEl.value === 'indicative') {
+    apiRequestUrl = `${API_URL}/swap/v0/price?buyToken=${BUY_TOKEN}&sellToken=${SELL_TOKEN}&sellAmount=${sellAmount}&takerAddress=${takerAddress}&slippagePerecent=${SLIPPAGE_PERCENT}`;
+  } else {
+    apiRequestUrl = `${API_URL}/swap/v0/quote?buyToken=${BUY_TOKEN}&sellToken=${SELL_TOKEN}&sellAmount=${sellAmount}&takerAddress=${takerAddress}&slippagePerecent=${SLIPPAGE_PERCENT}&skipValidation=true`;
+  }
+
+
+  const zeroExApiResponse = await window.fetch(apiRequestUrl, { headers: { "0x-api-key": apiKey } });
+  if (zeroExApiResponse.status === 200) {
+    resultsEl.innerText = JSON.stringify(await zeroExApiResponse.json(), null, 2);
+  } else {
+    resultsEl.innerText = `Error: ${zeroExApiResponse.status} ${zeroExApiResponse.body}`;
+  }
+
+
+
+  submitEl.disabled = false;
+
+
+
+};
